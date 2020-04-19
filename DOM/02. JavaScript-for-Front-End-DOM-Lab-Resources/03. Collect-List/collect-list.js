7f7b7e54bfe7271902315236fa217d3083359327
Ivan <igelov@hotmail.com>
2020-04-20 00:34:28 +0100

    Add/delete task

--- DOM/02. JavaScript-for-Front-End-DOM-Lab-Resources/03. Collect-List/collect-list.js
+++ DOM/02. JavaScript-for-Front-End-DOM-Lab-Resources/03. Collect-List/collect-list.js
@@ -4,5 +4,5 @@ function extractText() {
     for (let node of itemNodes) {
         textarea.value += node.textContent + "\n";
     }
-    
+
 }
--- DOM/02. JavaScript-for-Front-End-DOM-Lab-Resources/04. Add-Delete/app.js
+++ DOM/02. JavaScript-for-Front-End-DOM-Lab-Resources/04. Add-Delete/app.js
@@ -1,3 +1,18 @@
 function solve() {
-  // TODO:
+    let newElement = document.getElementById("newText").value; // Collect the Input field
+    let list = document.getElementById("items"); //Collect the List in the HTML
+    if (newElement.length === 0) return; //If the field is empty - do nothing;
+    let listItem = document.createElement("li"); //Start building a list item
+    listItem.textContent = newElement; //Else of the If, get the value of the input field.
+    let remove = document.createElement("a"); //Adding the remove link
+    let linkText = document.createTextNode("[Delete]");
+    remove.appendChild(linkText);
+    remove.href = "#";
+    remove.addEventListener("click", deleteItem);
+
+    listItem.appendChild(remove);
+    list.appendChild(listItem);
+    function deleteItem() {
+        listItem.remove();
+    }
 }
