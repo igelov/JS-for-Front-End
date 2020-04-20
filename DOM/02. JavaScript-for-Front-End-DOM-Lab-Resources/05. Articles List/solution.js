77c3fc61fd4ad68c3f16048eefb3948ab39b6cd9
Ivan <igelov@hotmail.com>
2020-04-20 23:32:15 +0100

    Article List

--- DOM/02. JavaScript-for-Front-End-DOM-Lab-Resources/05. Articles List/solution.js
+++ DOM/02. JavaScript-for-Front-End-DOM-Lab-Resources/05. Articles List/solution.js
@@ -1,4 +1,16 @@
 function createArticle() {
-	
-	//TODO...
+    let newElement = document.getElementById('createTitle');
+    let list = document.getElementById('createContent');
+    if ((newElement.value.trim().length === 0 || list.value.trim().length === 0)) return;
+    let article = document.createElement('article');
+    let title = document.createElement('h3');
+    title.textContent = newElement.value;
+    let context = document.createElement('p');
+    context.textContent = list.value;
+    article.appendChild(title);
+    article.appendChild(context);
+    let print = document.getElementById('articles');
+    print.appendChild(article);
+    newElement.value = "";
+    list.value = "";
 }
