dfe77a150bd27826fbb549f774dcd3323fafb957
Ivan <igelov@hotmail.com>
2020-04-21 01:30:48 +0100

    Format the text

--- DOM/02. JavaScript-for-Front-End-DOM-Lab-Resources/06. Format the Text/solution.js
+++ DOM/02. JavaScript-for-Front-End-DOM-Lab-Resources/06. Format the Text/solution.js
@@ -1,3 +1,17 @@
 function solve() {
-  //TODO...
+    let newText = document.getElementById('input').innerText;
+    let sentances = [];
+    sentances = newText.match(/[^\.!\?]+[\.!\?]+/g);
+    let par = [];
+    let cycle = sentances.length;
+    for (let i = 0; i < cycle; i++) {
+        par[i] = sentances.splice(0, 3).join('');
+    }
+    par = par.filter(item => item);//Removes the empty spaces
+    let paragraph = document.getElementById('output');
+    for (let i = 0; i < par.length; i++) {
+        let parHTML = document.createElement('p');
+        parHTML.textContent = par[i];
+        paragraph.appendChild(parHTML);
+    }
 }
