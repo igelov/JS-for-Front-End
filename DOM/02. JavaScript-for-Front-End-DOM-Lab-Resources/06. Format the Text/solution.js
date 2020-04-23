1592a3516d7fa412d73970ee4954dbf5bdf40d74
Ivan <igelov@hotmail.com>
2020-04-23 21:43:42 +0100

    Format the text_modern solution

--- DOM/02. JavaScript-for-Front-End-DOM-Lab-Resources/06. Format the Text/solution.js
+++ DOM/02. JavaScript-for-Front-End-DOM-Lab-Resources/06. Format the Text/solution.js
@@ -1,4 +1,24 @@
 function solve() {
+    const inputText = document.getElementById('input').innerText;
+    const outputElement = document.getElementById('output');
+
+    inputText
+        .match(/(?:\s*)([^.!?]+[.!?]+)/g)
+        .map(sentence => sentence.trim())
+        .reduce((acc, sentence, index) => {
+            if (index % 3 === 0) { acc.push([sentence]); } else { acc[acc.length - 1].push(sentence); }
+            return acc;
+        }, [])
+        .forEach(paragraph => {
+            let parHTML = document.createElement('p');
+            parHTML.textContent = paragraph;
+            outputElement.appendChild(parHTML);
+        });
+}
+
+
+/*
+function solve() {
     let newText = document.getElementById('input').innerText;
     let sentances = [];
     sentances = newText.match(/[^\.!\?]+[\.!\?]+/g);
@@ -8,10 +28,11 @@ function solve() {
         par[i] = sentances.splice(0, 3).join('');
     }
     par = par.filter(item => item);//Removes the empty spaces
+    //Bellow selects the output dif and appends each element of the array in a new <p>
     let paragraph = document.getElementById('output');
     for (let i = 0; i < par.length; i++) {
         let parHTML = document.createElement('p');
         parHTML.textContent = par[i];
         paragraph.appendChild(parHTML);
     }
-}
+}*/
